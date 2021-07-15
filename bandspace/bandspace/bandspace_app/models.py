542c6d3bcd8ff700e9a4bd1d270c8df80f8f8795
Skyler Vassallo <Skyler_Vassallo@glic.com>
2021-07-15 01:23:48 -0400

    Potential issues, but need to correct models for this program to work. Committing for now, will revert if need be.

--- bandspace/bandspace/bandspace_app/models.py
+++ bandspace/bandspace/bandspace_app/models.py
@@ -11,8 +11,7 @@ class User(models.Model):
         return self.name
 
 class Band(models.Model):
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bands', default=0)
     band_name = models.CharField(max_length=100)
     photo = models.TextField()
     about = models.CharField(max_length=200)
@@ -21,3 +20,10 @@ class Band(models.Model):
 
     def __str__(self):
         return self.band_name
+
+class UserFavBand(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='userfavbands')
+    band = models.ForeignKey(Band, on_delete=models.CASCADE, related_name='bandfavusers')
+
+    def __str__(self):
+        return self.band.band_name
